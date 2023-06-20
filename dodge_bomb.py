572aeedf794819ed8b0caadbfbe7966498babf94
manato <c0b22062c9@edu.teu.ac.jp>
2023-06-20 14:14:47 +0900

    練習Ⅰ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
+import random
 import sys
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -11,18 +13,30 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20, 20))  #練習Ⅰ
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    rect_bb_img = bb_img.get_rect()
+    rect_bb_img.center = x, y  
+    #爆弾Rectの位置を表す変数に乱数を設定する
+    bb_img.set_colorkey((0, 0, 0))
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+            
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img,rect_bb_img)
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+
+        clock.tick(50)
 
 
 if __name__ == "__main__":
